9e55d7c575d98a4497738d71a0e23e4d27137a96
Ahmed <ahmed@jsmastery.pro>
2025-12-23 11:27:25 +0200

    fixed prisma client import

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neonConfig } from "@neondatabase/serverless";
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 
 import ws from "ws";
 neonConfig.webSocketConstructor = ws;
