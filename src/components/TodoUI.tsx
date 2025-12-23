a81986a81f6fa0ddd39b266e4d9b3d1583079075
Ahmed <ahmed@jsmastery.pro>
2025-12-23 05:35:00 +0200

    added deleting todos to TodosCard

--- src/components/TodoUI.tsx
+++ src/components/TodoUI.tsx
@@ -1,33 +1,58 @@
+"use client";
+
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import TodosCard from "./TodosCard";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import { Input } from "./ui/input";
 
 type Todo = {
-  id: number;
+  id: string;
   content: string;
+  completed: boolean;
 };
 
-const todos: Todo[] = [
-  {
-    id: 1,
-    content: "Learn React basics",
-  },
-  {
-    id: 2,
-    content: "Build a Todo app with Next.js",
-  },
-  {
-    id: 3,
-    content: "Practice Tailwind CSS layouts",
-  },
-  {
-    id: 4,
-    content: "Push project to GitHub",
-  },
-];
-
 const TodoUI = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [content, setContent] = useState<string>("");
+
+  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ content }),
+      });
+
+      if (!res.ok) {
+        throw new Error("error creating todo");
+      }
+
+      if (res.ok) {
+        const data = await res.json();
+        setTodos([...todos, data.newTodo]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    setContent("");
+  };
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const res = await fetch("/api/todos");
+      const data = await res.json();
+      setTodos(data);
+    };
+
+    fetchTodos();
+  }, []);
+
   return (
     <div className="pt-20 space-y-5">
       <div className="text-center space-y-1">
@@ -36,14 +61,25 @@ const TodoUI = () => {
       </div>
       <Card className="w-full max-w-xl mx-auto shadow-lg">
         <CardContent className="space-y-6">
-          <form className="flex items-center gap-2">
-            <Input type="text" placeholder="Add a new task" />
+          <form onSubmit={handleSubmit} className="flex items-center gap-2">
+            <Input
+              type="text"
+              placeholder="Add a new task"
+              value={content}
+              onChange={handleChange}
+            />
             <Button variant={"outline"}>Add</Button>
           </form>
 
           <div className="flex flex-col gap-3 max-h-80 overflow-y-scroll">
             {todos.map((todo) => (
-              <TodosCard key={todo.id} content={todo.content} />
+              <TodosCard
+                key={todo.id}
+                todoId={todo.id}
+                content={todo.content}
+                todos={todos}
+                setTodos={setTodos}
+              />
             ))}
           </div>
         </CardContent>
--- src/components/TodosCard.tsx
+++ src/components/TodosCard.tsx
@@ -3,27 +3,61 @@
 import { DeleteIcon, EditIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
+import { toast } from "sonner";
 
-type Props = {
-  id?: number;
+type Todo = {
+  id: string;
   content: string;
+  completed: boolean;
 };
 
-const TodosCard = ({ content }: Props) => {
+type Props = {
+  todoId: string;
+  content: string;
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  todos: Todo[];
+};
+
+const TodosCard = ({ content, todoId, todos, setTodos }: Props) => {
+  const handleDelete = async () => {
+    const res = await fetch(`/api/todos/${todoId}`, {
+      method: "DELETE",
+    });
+
+    if (!res.ok) {
+      throw new Error("Error deleting todo");
+    }
+
+    if (res.ok) {
+      toast.success("Task deleted successfully");
+      setTodos(todos.filter((todo) => todo.id !== todoId));
+    }
+  };
   return (
     <div className="border rounded-xl flex items-center justify-between p-4">
       <div className="flex items-center gap-3">
-        <Checkbox id="completed" />
-        <label htmlFor="completed" className="wrap-break-word">
+        <Checkbox id={todoId} />
+        <label htmlFor={todoId} className="wrap-break-word">
           {content}
         </label>
       </div>
 
       <div className="flex gap-2">
-        <Button size={"icon"} variant={"outline"} title="edit">
+        <Button
+          size={"icon"}
+          variant={"outline"}
+          title="edit"
+          className="cursor-pointer"
+        >
           <EditIcon />
         </Button>
-        <Button variant={"outline"} size={"icon"} title="delete">
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          title="delete"
+          onClick={handleDelete}
+          className="cursor-pointer"
+        >
           <DeleteIcon className="text-red-500" />
         </Button>
       </div>
